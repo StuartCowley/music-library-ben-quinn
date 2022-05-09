137652ef4ff091a008403ce4197f817175672b58
Ben Quinn <benjamesquinn@gmail.com>
2022-05-09 10:50:40 +0100

    Added middlewares

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # Music Library API :notes:
+
 A simple API that allows users to perform CRUD operations on a Music Library.
 
 ## Languages
+
 - Javascript
 - MySQL
 - Express.js
-
-
-
-
-
-
--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const artistRoutes = require('./routes/artist');
 const albumRoutes = require('./routes/album');
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 
 // Artist Routes
 app.use('/artist', artistRoutes);
